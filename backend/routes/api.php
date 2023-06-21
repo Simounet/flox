7b84b11f2fade21b3ed964333ac409a1dd3127ef
Simounet <contact@simounet.net>
2023-06-21 23:14:26 +0200

    Mandatory API route prefix removed

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-  Route::group(['middleware' => 'auth.basic'], function() {
-    Route::patch('/update-files', 'FileParserController@receive');
-    Route::post('/import', 'ExportImportController@import');
-    Route::get('/export', 'ExportImportController@export');
-  });
+  Route::group(['prefix' => 'api'], function() {
+    Route::group(['middleware' => 'auth.basic'], function () {
+        Route::patch('/update-files', 'FileParserController@receive');
+        Route::post('/import', 'ExportImportController@import');
+        Route::get('/export', 'ExportImportController@export');
+    });
 
-  Route::get('/last-fetched', 'FileParserController@lastFetched');
+    Route::get('/last-fetched', 'FileParserController@lastFetched');
+  });
