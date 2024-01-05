2ddf538f1bee42da9c03dee29eb0e08687aaec5b
Simounet <contact@simounet.net>
2024-01-05 22:16:49 +0100

    changeRating method added to ReviewService

--- backend/app/Services/Models/ReviewService.php
+++ backend/app/Services/Models/ReviewService.php
@@ -2,6 +2,10 @@
 
 namespace App\Services\Models;
 
+use App\Models\Item;
+use App\Models\Review;
+use Symfony\Component\HttpFoundation\Response;
+
 class ReviewService {
 
     public function getRating(int $rating): string
@@ -16,5 +20,32 @@ class ReviewService {
             default:
                 return 'not rated';
         }
+    }
+
+    public function changeRating(int $reviewId, int $rating): Response
+    {
+      $review = Review::find($reviewId);
+
+      if(! $review) {
+        return response('Not Found', Response::HTTP_NOT_FOUND);
+      }
+
+      // Update the parent relation only if we change rating from neutral.
+      if($review->rating == 0) {
+        // @TODO weird to update it with tmdb_id
+        // @TODO last_seen column should be moved from the items table
+        // $item->updateLastSeenAt($item->tmdb_id);
+      }
+
+      $updatedReview = $review->update([
+        'rating' => $rating,
+        'watchlist' => false,
+      ]);
+
+      if($updatedReview === false) {
+          return response('Review not updated: ' . $reviewId, Response::HTTP_INTERNAL_SERVER_ERROR);
+      }
+
+      return response('', Response::HTTP_OK);
     }
 }
