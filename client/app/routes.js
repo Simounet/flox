d8c45083990fcbfcd2d3d47a7b2b6cd3298211eb
Simounet <contact@simounet.net>
2024-03-03 22:58:12 +0100

    Vue3: upgrading vue-router from 3 to 4

--- client/app/routes.js
+++ client/app/routes.js
@@ -1,12 +1,11 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import config from './config';
+import config from "./config";
 
-import Content from './components/Content/Content.vue';
-import SearchContent from './components/Content/SearchContent.vue';
-import TMDBContent from './components/Content/TMDBContent.vue';
-import Subpage from './components/Content/Subpage.vue';
+import Content from "./components/Content/Content.vue";
+import SearchContent from "./components/Content/SearchContent.vue";
+import TMDBContent from "./components/Content/TMDBContent.vue";
+import Subpage from "./components/Content/Subpage.vue";
 import ReviewPage from "./components/Content/ReviewPage.vue";
 
 const Calendar = () =>
@@ -18,33 +17,44 @@ const Settings = () =>
     /* webpackChunkName: "settings" */ "./components/Content/Settings/Index.vue"
   );
 
-Vue.use(Router);
-
-export default new Router({
-  mode: 'history',
-  base: config.uri,
+export default createRouter({
+  history: createWebHistory(config.uri),
   routes: [
-    { path: '/', component: Content, name: 'home' },
+    { path: "/", component: Content, name: "home" },
 
     // todo: use props for media type
-    { path: '/movies', component: Content, name: 'movie' },
-    { path: '/tv', component: Content, name: 'tv' },
-    { path: '/watchlist/:type?', component: Content, name: 'watchlist' },
+    { path: "/movies", component: Content, name: "movie" },
+    { path: "/tv", component: Content, name: "tv" },
+    { path: "/watchlist/:type?", component: Content, name: "watchlist" },
 
-    { path: '/users/:username/review/:reviewId', component: ReviewPage, name: 'user-review' },
-    
-    { path: '/movies/:tmdbId/:slug?', component: Subpage, name: 'subpage-movie', props: {mediaType: 'movie'} },
-    { path: '/tv/:tmdbId/:slug?', component: Subpage, name: 'subpage-tv', props: {mediaType: 'tv'} },
+    {
+      path: "/users/:username/review/:reviewId",
+      component: ReviewPage,
+      name: "user-review",
+    },
 
-    { path: '/search', component: SearchContent, name: 'search' },
+    {
+      path: "/movies/:tmdbId/:slug?",
+      component: Subpage,
+      name: "subpage-movie",
+      props: { mediaType: "movie" },
+    },
+    {
+      path: "/tv/:tmdbId/:slug?",
+      component: Subpage,
+      name: "subpage-tv",
+      props: { mediaType: "tv" },
+    },
+
+    { path: "/search", component: SearchContent, name: "search" },
     { path: "/settings", component: Settings, name: "settings" },
-    { path: '/suggestions', component: TMDBContent, name: 'suggestions' },
-    { path: '/trending', component: TMDBContent, name: 'trending' },
-    { path: '/upcoming', component: TMDBContent, name: 'upcoming' },
-    { path: '/now-playing', component: TMDBContent, name: 'now-playing' },
-    { path: '/genre/:genre', component: TMDBContent, name: 'genre' },
+    { path: "/suggestions", component: TMDBContent, name: "suggestions" },
+    { path: "/trending", component: TMDBContent, name: "trending" },
+    { path: "/upcoming", component: TMDBContent, name: "upcoming" },
+    { path: "/now-playing", component: TMDBContent, name: "now-playing" },
+    { path: "/genre/:genre", component: TMDBContent, name: "genre" },
     { path: "/calendar", component: Calendar, name: "calendar" },
 
-    { path: '*', redirect: '/' }
-  ]
+    { path: "/:pathMatch(.*)", redirect: "/" },
+  ],
 });
