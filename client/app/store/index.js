ac8421d3ea41b76ca333b5229a845fe23c6ec011
Simounet <contact@simounet.net>
2024-03-04 22:24:57 +0100

    [Boyscout] Lint

--- client/app/store/index.js
+++ client/app/store/index.js
@@ -1,37 +1,37 @@
-import Vue from 'vue';
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import * as actions from './actions';
-import mutations from './mutations';
+import * as actions from "./actions";
+import mutations from "./mutations";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     filters: [
-      'last seen',
-      'own rating',
-      'title',
-      'release',
-      'tmdb rating',
-      'imdb rating'
+      "last seen",
+      "own rating",
+      "title",
+      "release",
+      "tmdb rating",
+      "imdb rating",
     ],
     showFilters: false,
     items: [],
-    searchTitle: '',
-    userFilter: '',
-    userSortDirection: '',
+    searchTitle: "",
+    userFilter: "",
+    userSortDirection: "",
     loading: false,
     clickedMoreLoading: false,
     paginator: null,
-    colorScheme: '',
+    colorScheme: "",
     overlay: false,
     modalData: {},
     loadingModalData: true,
     seasonActiveModal: 1,
-    modalType: '',
-    itemLoadedSubpage: false
+    modalType: "",
+    itemLoadedSubpage: false,
   },
   mutations,
-  actions
+  actions,
 });
