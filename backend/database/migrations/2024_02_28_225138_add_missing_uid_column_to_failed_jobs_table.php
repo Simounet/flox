89e2006ab271fc6d9dc3e947252abf8c3344cc02
Simounet <contact@simounet.net>
2024-03-06 22:01:45 +0100

    Missing uuid column added to failed_jobs table

---
+++ backend/database/migrations/2024_02_28_225138_add_missing_uid_column_to_failed_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->string('uuid')->unique()->nullable()->after('id');
+        });
+
+        $failedJobs = DB::table('failed_jobs')->get();
+
+        foreach ($failedJobs as $failedJob) {
+            $uuid = (string) Str::uuid();
+            DB::table('failed_jobs')
+                ->where('id', $failedJob->id)
+                ->update(['uuid' => $uuid]);
+        }
+
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->string('uuid')->nullable(false)->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+};
