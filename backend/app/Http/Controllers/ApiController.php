bd2364e6059d50bc8d22ddb6133642967e401566
Simounet <contact@simounet.net>
2024-09-13 22:29:48 +0200

    Kodi API added

--- backend/app/Http/Controllers/ApiController.php
+++ backend/app/Http/Controllers/ApiController.php
@@ -3,19 +3,37 @@
   namespace App\Http\Controllers;
 
   use App\Enums\StatusEnum;
+  use App\Services\Api\Kodi;
   use App\Services\Api\Plex;
+  use Illuminate\Http\Request;
   use Illuminate\Http\Response;
 
   class ApiController {
 
-    /**
-     * @var Plex
-     */
-    private $plex;
+    public function __construct(
+      private Kodi $kodi,
+      private Plex $plex
+    ) {}
 
-    public function __construct(Plex $plex)
+    public function kodi(Request $request): Response
     {
-      $this->plex = $plex;
+      $payload = $request->all();
+
+      $result = $this->kodi->handle($payload);
+
+      if($result === StatusEnum::NOT_FOUND) {
+        return response('Not Found', 404);
+      }
+
+      if($result === StatusEnum::NOT_IMPLEMENTED) {
+        return response(501, Response::HTTP_NOT_IMPLEMENTED);
+      }
+
+      if($result === StatusEnum::UNAUTHORIZED) {
+        return response(501, Response::HTTP_UNAUTHORIZED);
+      }
+
+      return response('Ok', 200);
     }
 
     public function plex(): Response
