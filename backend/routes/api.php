72236665efb18a86051274e8197b16242843cc94
Simounet <contact@simounet.net>
2024-11-20 21:08:06 +0100

    Route definition using classes instead of strings

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,22 +1,27 @@
 <?php
 
+use App\Http\Controllers\ActorController;
+use App\Http\Controllers\ExportImportController;
+use App\Http\Controllers\FileParserController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\WebFingerController;
 use Illuminate\Support\Facades\Route;
 
-  Route::group(['prefix' => 'api'], function() {
+Route::group(['prefix' => 'api'], function() {
     Route::group(['middleware' => 'auth.basic'], function () {
-        Route::patch('/update-files', 'FileParserController@receive');
-        Route::post('/import', 'ExportImportController@import');
-        Route::get('/export', 'ExportImportController@export');
+        Route::patch('/update-files', [FileParserController::class, 'receive']);
+        Route::post('/import', [ExportImportController::class, 'import']);
+        Route::get('/export', [ExportImportController::class, 'export']);
     });
 
-    Route::get('/last-fetched', 'FileParserController@lastFetched');
-    Route::get('/review/{id}', 'ReviewController@show');
-  });
+    Route::get('/last-fetched', [FileParserController::class, 'lastFetched']);
+    Route::get('/review/{id}', [ReviewController::class, 'show']);
+});
 
-  Route::get('.well-known/webfinger', 'WebFingerController@handle')->name('well-known.webfinger');
-  Route::get('/users/{username}/followers', 'ActorController@followers')->name('federation.user.followers');
-  Route::get('/users/{username}/following', 'ActorController@following')->name('federation.user.following');
-  Route::post('/users/{username}/inbox', 'ActorController@inbox')->name('federation.user.inbox');
-  Route::get('/users/{username}/outbox', 'ActorController@outbox')->name('federation.user.outbox');
-  Route::get('/users/{username}', 'ActorController@actor')->name('federation.user');
-  Route::post('/inbox', 'ActorController@sharedInbox')->name('federation.shared-inbox');
+Route::get('.well-known/webfinger', [WebFingerController::class, 'handle'])->name('well-known.webfinger');
+Route::get('/users/{username}/followers', [ActorController::class, 'followers'])->name('federation.user.followers');
+Route::get('/users/{username}/following', [ActorController::class, 'following'])->name('federation.user.following');
+Route::post('/users/{username}/inbox', [ActorController::class, 'inbox'])->name('federation.user.inbox');
+Route::get('/users/{username}/outbox', [ActorController::class, 'outbox'])->name('federation.user.outbox');
+Route::get('/users/{username}', [ActorController::class, 'actor'])->name('federation.user');
+Route::post('/inbox', [ActorController::class, 'sharedInbox'])->name('federation.shared-inbox');
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,70 +1,82 @@
 <?php
 
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\FileParserController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\SubpageController;
+use App\Http\Controllers\TMDBController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VideoController;
+use App\Http\Middleware\JsonRequestOnly;
 use Illuminate\Support\Facades\Route;
-  use App\Http\Middleware\JsonRequestOnly;
 
-  Route::prefix('api')->group(function() {
-    Route::get('/logout', 'UserController@logout');
-    Route::post('/login', 'UserController@login');
+Route::prefix('api')->group(function() {
+    Route::get('/logout', [UserController::class, 'logout']);
+    Route::post('/login', [UserController::class, 'login']);
 
-    Route::get('/items/{type}/{orderBy}/{sortDirection}', 'ItemController@items');
-    Route::get('/search-items', 'ItemController@search');
+    Route::get('/items/{type}/{orderBy}/{sortDirection}', [ItemController::class, 'items']);
+    Route::get('/search-items', [ItemController::class, 'search']);
 
-    Route::get('/item/{tmdbId}/{mediaType}', 'SubpageController@item');
-    Route::get('/imdb-rating/{imdbId}', 'SubpageController@imdbRating');
+    Route::get('/item/{tmdbId}/{mediaType}', [SubpageController::class, 'item']);
+    Route::get('/imdb-rating/{imdbId}', [SubpageController::class, 'imdbRating']);
 
-    Route::get('/suggestions/{tmdbID}/{mediaType}', 'TMDBController@suggestions');
-    Route::get('/genres', 'GenreController@allGenres');
-    Route::get('/genre/{genre}', 'TMDBController@genre');
-    Route::get('/trending', 'TMDBController@trending');
-    Route::get('/upcoming', 'TMDBController@upcoming');
-    Route::get('/now-playing', 'TMDBController@nowPlaying');
+    Route::get('/suggestions/{tmdbID}/{mediaType}', [TMDBController::class, 'suggestions']);
+    Route::get('/genres', [GenreController::class, 'allGenres']);
+    Route::get('/genre/{genre}', [TMDBController::class, 'genre']);
+    Route::get('/trending', [TMDBController::class, 'trending']);
+    Route::get('/upcoming', [TMDBController::class, 'upcoming']);
+    Route::get('/now-playing', [TMDBController::class, 'nowPlaying']);
 
-    Route::patch('/refresh-all', 'ItemController@refreshAll');
-    Route::get('/settings', 'SettingController@settings');
+    Route::patch('/refresh-all', [ItemController::class, 'refreshAll']);
+    Route::get('/settings', [SettingController::class, 'settings']);
 
     Route::middleware('api_key')->group(function() {
-      Route::post('kodi', 'ApiController@kodi');
-      Route::post('plex', 'ApiController@plex');
+        Route::post('kodi', [ApiController::class, 'kodi']);
+        Route::post('plex', [ApiController::class, 'plex']);
     });
 
     Route::middleware('auth')->group(function() {
-      Route::get('/calendar', 'CalendarController@items');
-      Route::get('/check-update', 'SettingController@checkForUpdate');
-      Route::get('/episodes/{tmdbId}', 'ItemController@episodes');
-      Route::get('/version', 'SettingController@getVersion');
-      Route::get('/api-key', 'SettingController@getApiKey');
-      Route::patch('/settings/refresh', 'SettingController@updateRefresh');
-      Route::patch('/settings/api-key', 'SettingController@generateApiKey');
-      Route::patch('/settings/reminders-send-to', 'SettingController@updateRemindersSendTo');
-      Route::patch('/settings/reminder-options', 'SettingController@updateReminderOptions');
-      Route::patch('/settings', 'SettingController@updateSettings');
+        Route::get('/calendar', [CalendarController::class, 'items']);
+        Route::get('/check-update', [SettingController::class, 'checkForUpdate']);
+        Route::get('/episodes/{tmdbId}', [ItemController::class, 'episodes']);
+        Route::get('/version', [SettingController::class, 'getVersion']);
+        Route::get('/api-key', [SettingController::class, 'getApiKey']);
+        Route::patch('/settings/refresh', [SettingController::class, 'updateRefresh']);
+        Route::patch('/settings/api-key', [SettingController::class, 'generateApiKey']);
+        Route::patch('/settings/reminders-send-to', [SettingController::class, 'updateRemindersSendTo']);
+        Route::patch('/settings/reminder-options', [SettingController::class, 'updateReminderOptions']);
+        Route::patch('/settings', [SettingController::class, 'updateSettings']);
 
-      Route::post('/add', 'ItemController@add');
-      Route::post('/watchlist', 'ItemController@watchlist');
-      Route::patch('/update-alternative-titles/{tmdbId?}', 'ItemController@updateAlternativeTitles');
-      Route::patch('/update-genre', 'ItemController@updateGenre');
-      Route::patch('/toggle-episode/{id}', 'ItemController@toggleEpisode');
-      Route::patch('/toggle-season', 'ItemController@toggleSeason');
-      Route::patch('/refresh/{itemId}', 'ItemController@refresh');
+        Route::post('/add', [ItemController::class, 'add']);
+        Route::post('/watchlist', [ItemController::class, 'watchlist']);
+        Route::patch('/update-alternative-titles/{tmdbId?}', [ItemController::class, 'updateAlternativeTitles']);
+        Route::patch('/update-genre', [ItemController::class, 'updateGenre']);
+        Route::patch('/toggle-episode/{id}', [ItemController::class, 'toggleEpisode']);
+        Route::patch('/toggle-season', [ItemController::class, 'toggleSeason']);
+        Route::patch('/refresh/{itemId}', [ItemController::class, 'refresh']);
 
-      Route::get('/userdata', 'UserController@getUserData');
-      Route::patch('/userdata', 'UserController@changeUserData')->middleware('csrf');
+        Route::get('/userdata', [UserController::class, 'getUserData']);
+        Route::patch('/userdata', [UserController::class, 'changeUserData'])->middleware('csrf');
 
-      Route::patch('/review/change-rating/{reviewId}', 'ReviewController@changeRating');
-      Route::delete('/review/{id}', 'ReviewController@delete')->middleware('csrf');
-      Route::post('/review', 'ReviewController@store')->middleware('csrf');
+        Route::patch('/review/change-rating/{reviewId}', [ReviewController::class, 'changeRating']);
+        Route::delete('/review/{id}', [ReviewController::class, 'delete'])->middleware('csrf');
+        Route::post('/review', [ReviewController::class, 'store'])->middleware('csrf');
 
-      Route::get('/search-tmdb', 'TMDBController@search');
+        Route::get('/search-tmdb', [TMDBController::class, 'search']);
 
-      Route::post('/fetch-files', 'FileParserController@call');
+        Route::post('/fetch-files', [FileParserController::class, 'call']);
 
-      Route::get('/video/{type}/{id}', 'VideoController@serve');
+        Route::get('/video/{type}/{id}', [VideoController::class, 'serve']);
     });
-  });
+});
 
-  Route::middleware([JsonRequestOnly::class])->group(function () {
-      Route::get('/users/{username}/review/{id}', 'ReviewController@showObject')->name('user.review');
-  });
+Route::middleware([JsonRequestOnly::class])->group(function () {
+    Route::get('/users/{username}/review/{id}', [ReviewController::class, 'showObject'])->name('user.review');
+});
 
-  Route::fallback('HomeController@app');
+Route::fallback([HomeController::class, 'app']);
