e57f02f308975b0cdb8795a2a736ef37375296a5
Simounet <contact@simounet.net>
2025-03-11 22:16:09 +0100

    [Client] element-plus @element-plus/theme-chalk dependencies removed

--- client/app/app.js
+++ client/app/app.js
@@ -3,9 +3,6 @@ require("../resources/sass/app.scss");
 import { createApp } from "vue";
 import VueHotkey from "v-hotkey3";
 
-import { ElCheckbox } from "element-plus";
-import 'element-plus/dist/index.css'
-
 import App from "./App.vue";
 import router from "./routes";
 import store from "./store";
@@ -17,6 +14,5 @@ app.config.globalProperties.$store = store;
 app.use(router);
 app.use(store);
 app.use(VueHotkey);
-app.use(ElCheckbox);
 
 app.mount("#app");
